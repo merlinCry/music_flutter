3d5b4bc3f3058ca69905a768663cfbe1e8c19fb7
songqijian <songqijian@daqo.com>
2021-11-09 15:18:12 +0800

    项目导入

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:music_flutter/utils.dart';
+
+void main() {
+  runApp(const MusicApp());
+}
+
+class MusicApp extends StatelessWidget {
+  const MusicApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'music',
+      theme: ThemeData(
+        primarySwatch: mainColor,
+      ),
+      home: const MusicHomePage(title: 'music'),
+    );
+  }
+}
+
+class MusicHomePage extends StatefulWidget {
+  const MusicHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MusicHomePage> createState() => _MusicHomePageState();
+}
+
+class _MusicHomePageState extends State<MusicHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: Container(),
+      )
+    );
+  }
+}
---
+++ lib/utils.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+Map<int, Color> color =
+{
+  50: const Color.fromRGBO(221,0,27, .1),
+  100: const Color.fromRGBO(221,0,27, .2),
+  200: const Color.fromRGBO(221,0,27, .3),
+  300: const Color.fromRGBO(221,0,27, .4),
+  400: const Color.fromRGBO(221,0,27, .5),
+  500: const Color.fromRGBO(221,0,27, .6),
+  600: const Color.fromRGBO(221,0,27, .7),
+  700: const Color.fromRGBO(221,0,27, .8),
+  800: const Color.fromRGBO(221,0,27, .9),
+  900: const Color.fromRGBO(221,0,27, 1),
+};
+
+MaterialColor mainColor = MaterialColor(0xFFDD001B, color);
